47fa8c3665eb4cf787ae86924ec16265a0e7bd22
Rahn <bombizzen@hotmail.com>
2020-10-28 15:11:09 +0100

    lol
    g

---
+++ PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    public int score = 0;
+
+    public Text scoreText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        score++;
+        Destroy(other.gameObject);
+
+        scoreText.text = "Score: " + score;
+    }
+
+
+}
